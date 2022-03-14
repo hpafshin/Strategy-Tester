b1b65314403a8d412895e53c5a1ccb85cae606bd
hpafshin <93883555+hpafshin@users.noreply.github.com>
2022-03-14 23:55:11 +0330

    Adding # Code description

--- Raha1 Strategy.py
+++ Raha1 Strategy.py
@@ -1,14 +1,17 @@
+# Importing required libraries
 import pandas as pd
 import numpy as np
 import pathlib
 
+# Reading every file in a folder with at least 21 records
 path = pathlib.Path('e:/TSE Data/LatinSymbol')
 final_list =[]
 for file in path.iterdir():
     df = pd.read_csv (file)
     if len(df)<21:
         pass
-    else:    
+    else:
+# Defining indicators functions    
         def williams_aligator (jaw, teeth, lip):
             df ['mean'] = (df['High'] + df ['Low'])/2
             def SMMA (period):
@@ -25,7 +28,6 @@ for file in path.iterdir():
             df ['teeth'] = df [f'smma{teeth}'] .shift (5)
             df ['lip'] = df [f'smma{lip}'] .shift (3)
             return df    
-
         def macd (fast, slow, signal):
             exp1 = df.ewm (span= fast, adjust=False) ['Close'].mean ()
             exp2 = df.ewm (span= slow, adjust=False) ['Close'].mean ()
@@ -36,32 +38,34 @@ for file in path.iterdir():
             histogram = macd - sig
             df ['Histogram'] = histogram
             return df
-
         df = williams_aligator (13, 8, 5)
         df = macd (12,26,9)
-
+# Generating Strategy with buy and sell signals
         df['MACDsig'] = np.where ((np.logical_and(df['MACD']>0, df['MACD']>df['Signal'])), True, False)
         df['Gatorsig'] = np.where ((np.logical_and(df['lip']>df['teeth'], df['teeth']>df['jaw'])), True, False)
         df['Buy'] = np.where ((np.logical_and(df['MACDsig']==True, df['Gatorsig']==True)),'buy','-')
         df['Sell 1/2'] = np.where ((np.logical_and(df['MACD']<df['Signal'], df['Gatorsig']==False)),'sell 1/2','-')
         df['Sell All'] = np.where ((df['MACD']<0),'sell all', '-')
 
-        deposit = 0
+# Back testing the strategy
+        deposit_state = 0
+        number_of_transactions = 0
         money = 10000000
         shares = 0
-        for i in range (len(df)):
-            if df.loc [i,'Buy'] == 'buy' and deposit == 0:
-                deposit = 1
-                shares = int(money/df.loc[i,'AdjClose'])
-                money -= shares *df.loc[i,'AdjClose']
-            elif df.loc [i,'Sell All'] == 'sell all' and deposit != 0:
-                deposit = 0
-                money += shares * df.loc[i,'AdjClose']
-                shares = 0
-                    
-        money += shares * df.loc[i,'AdjClose']
-        temp_list = [df.loc[1,'Symbol'], money]
+        for i in range (len(df)-1):
+            if df.loc [i,'Buy'] == 'buy' and deposit_state == 0:
+                deposit_state = 1
+                number_of_transactions += 1
+                shares = int(money/df.loc[i+1,'Open'])
+                money -= shares *df.loc[i+1,'Open']
+            elif df.loc [i,'Sell All'] == 'sell all' and deposit_state != 0:
+                deposit_state = 0
+                money += shares * df.loc[i+1,'Open']
+                shares = 0            
+        money += shares * df.loc[i+1,'Open']
+        strategy_efficiency = money/10000000
+        temp_list = [df.loc[1,'Symbol'], strategy_efficiency, number_of_transactions]
         final_list.append (temp_list)
-
+# Exporting result to a .csv file
 data_frame = pd.DataFrame (final_list)
 data_frame.to_csv ('Raha1_Sanat.csv', index=False)
